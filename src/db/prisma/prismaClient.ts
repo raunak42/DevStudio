6e420eafc7165bea97f0861bf0f68c94e16e7664
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-18 04:20:16 +0530

    Instantiating and supplying prisma client in a global variable.

--- src/db/prisma/prismaClient.ts
+++ src/db/prisma/prismaClient.ts
@@ -1,17 +1,14 @@
+/**Frameworks like Next.js support hot reloading of changed files, which enables you to see changes to your application without restarting. However, if the framework refreshes the module responsible for exporting PrismaClient, this can result in additional, unwanted instances of PrismaClient in a development environment.
+
+As a workaround, you can store PrismaClient as a global variable in development environments only, as global variables are not reloaded.
+
+Why only in development environment? Because in production environment there is no hot reloading since hot reloading takes place only if the code changes during the running of the application.*/
+
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-async function main() {
-    
-}
+export const prisma =
+    globalForPrisma.prisma || new PrismaClient()
 
-main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
