14961bfb42ec8b66e2629c5e3ff1cee972b7a553
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-20 19:23:44 +0530

    Dashboard created/

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Page(){
-    return<div>sexy</div>
+"use client";
+
+import CreateProjectModal from "@/app/(pages)/dashboard/CreateProjectModal";
+import { CreateNew } from "./CreateNew";
+import { useState } from "react";
+
+export default function Page() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  return (
+    <div className="w-full min-h-screen h-full bg-gray-100 flex flex-col items-center justify-start py-12 px-28 relative">
+      <CreateNew setShowModal={setShowModal} />
+      <CreateProjectModal showModal={showModal} setShowModal={setShowModal} />
+
+      <div className="w-full flex flex-col items-start justify-start mt-12 gap-4">
+        <h1 className="text-[#000000] text-xl font-semibold">Your projects</h1>
+        <div className="w-[400px] h-[60px] rounded-2xl border border-dashed border-[#000000]"></div>
+      </div>
+    </div>
+  );
 }
