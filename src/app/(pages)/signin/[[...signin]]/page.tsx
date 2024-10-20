19b3ce4d8f83c30ecbcc5e4f965b5eda291faa0c
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-20 19:23:06 +0530

    Redirect urls configured.

--- src/app/(pages)/signin/[[...signin]]/page.tsx
+++ src/app/(pages)/signin/[[...signin]]/page.tsx
@@ -3,7 +3,12 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="h-[90vh] w-screen flex items-start justify-center overflow-x-clip mt-12">
-      <SignIn fallbackRedirectUrl={"/dashboard"} />
+      <SignIn
+        fallbackRedirectUrl={"/dashboard"}
+        forceRedirectUrl={"/dashboard"}
+        signUpForceRedirectUrl={"/dashboard"}
+        signUpFallbackRedirectUrl={"/dashboard"}
+      />
     </div>
   );
 }
--- src/app/(pages)/signup/[[...signup]]/page.tsx
+++ src/app/(pages)/signup/[[...signup]]/page.tsx
@@ -3,7 +3,12 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="h-[90vh] w-screen flex items-start justify-center overflow-x-clip mt-12">
-      <SignUp signInFallbackRedirectUrl={"/dashboard"} />
+      <SignUp
+        signInFallbackRedirectUrl={"/dashboard"}
+        fallbackRedirectUrl={"/dashboard"}
+        forceRedirectUrl={"/dashboard"}
+        signInForceRedirectUrl={"/dashboard"}
+      />
     </div>
   );
 }
