eedd32f917ef068c593e84dd03faa50be6049770
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-24 22:56:15 +0530

    Initialized socet server.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,17 +1,22 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import { createServer } from "http";
 import { httpHandler } from "./httpHandler";
+import { socketHandler } from "./ws";
 
 dotenv.config();
-const port = 3001
 
+const port = 3001
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-httpHandler(app)
+const httpServer = createServer(app)
 
-app.listen(port, () => {
+httpHandler(app)
+socketHandler(httpServer)
+
+httpServer.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
