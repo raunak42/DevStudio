dc8cebc5efaef43fcce505540d83f2dd52ec4206
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-24 22:55:30 +0530

    Logic added to fetch s3 folder to the local machine and then emit an event(files-loaded) that allows the requestPty event to run.

--- apps/backend/src/aws.ts
+++ apps/backend/src/aws.ts
@@ -1,21 +1,22 @@
-import { CopyObjectCommand, ListObjectsV2Command, S3Client, type CopyObjectCommandInput, type ListObjectsV2CommandInput } from '@aws-sdk/client-s3';
+import { CopyObjectCommand, ListObjectsV2Command, S3Client, type CopyObjectCommandInput, type ListObjectsV2CommandInput, GetObjectOutput, GetObjectCommandInput, GetObjectCommand } from '@aws-sdk/client-s3';
+import fs from "fs";
+import path from "path";
 
 
 const s3Client = new S3Client({
     region: 'eu-central-1', // e.g., 'us-west-2'
 });
 
-export const copyS3Folder = async (frameWork: string, userId: string, replId:string) => {
+export const copyS3Folder = async (frameWork: string, userId: string, replId: string, continuationToken?: string) => {
     const bucket = process.env.S3_BUCKET
-    // const replId = getRandomSlug()
-
     const sourcePrefix = `base/${frameWork}`
-    const destinationPrefix = `user/${userId}/${frameWork}-${replId}`
+    const destinationPrefix = `user/${userId}/${replId}`
 
     try {
         const listParams: ListObjectsV2CommandInput = {
             Bucket: bucket,
             Prefix: sourcePrefix,
+            ContinuationToken: continuationToken
         };
         const listCommand = new ListObjectsV2Command(listParams);
         const listedObjects = await s3Client.send(listCommand);
@@ -27,17 +28,22 @@ export const copyS3Folder = async (frameWork: string, userId: string, replId:str
             }
         }
 
-        await Promise.all(listedObjects.Contents.map(async (object) => {
-            if (!object.Key) return
-            const destinationKey = object.Key.replace(sourcePrefix, destinationPrefix)
+        await Promise.all(listedObjects.Contents.map(async (file) => {
+            if (!file.Key) return
+            const destinationKey = file.Key.replace(sourcePrefix, destinationPrefix)
             const copyParams: CopyObjectCommandInput = {
                 Bucket: bucket,
-                CopySource: `${bucket}/${object.Key}`,
+                CopySource: `${bucket}/${file.Key}`,
                 Key: destinationKey
             }
             const copyCommand = new CopyObjectCommand(copyParams);
             await s3Client.send(copyCommand)
         }))
+
+        if (listedObjects.IsTruncated) {
+            listParams.ContinuationToken = listedObjects.NextContinuationToken;
+            await copyS3Folder(frameWork, userId, replId, continuationToken)
+        }
 
         return {
             message: "Copy operations completed.",
@@ -46,8 +52,88 @@ export const copyS3Folder = async (frameWork: string, userId: string, replId:str
     } catch (error) {
         console.error('Error transferring folder:', error);
         return {
-            message:'Failed to transfer folder',
-            status:500
+            message: 'Failed to transfer folder',
+            status: 500
         }
     }
+}
+
+export const fetchS3Folder = async (key: string, localPath: string) => {
+    const bucket = process.env.S3_BUCKET
+
+    try {
+        const listParams: ListObjectsV2CommandInput = {
+            Bucket: bucket,
+            Prefix: key,
+        };
+
+        const listCommand = new ListObjectsV2Command(listParams);
+        const listedObjects = await s3Client.send(listCommand);
+
+        if (!listedObjects.Contents || listedObjects.Contents.length === 0) {
+            return {
+                message: "No objects found in the source folder.",
+                status: 404
+            }
+        }
+        await Promise.all(listedObjects.Contents.map(async (file) => {
+            if (!file.Key) return
+            const getObjectParams: GetObjectCommandInput = {
+                Bucket: bucket,
+                Key: file.Key
+            }
+            const getObjectsCommand = new GetObjectCommand(getObjectParams);
+            const data: GetObjectOutput = await s3Client.send(getObjectsCommand)
+            if (!data) return
+            const streamToBuffer = async (stream: any): Promise<Buffer> => {
+                const chunks: Buffer[] = [];
+                return new Promise((resolve, reject) => {
+                    stream.on('data', (chunk: Buffer) => chunks.push(chunk));
+                    stream.on('error', reject);
+                    stream.on('end', () => resolve(Buffer.concat(chunks)));
+                });
+            };
+
+            const fileData = await streamToBuffer(data.Body);
+            const filePath = `${localPath}/${file.Key.replace(key, "")}`;
+            await writeFile(filePath, fileData);
+        }))
+
+        return {
+            message: "Download completed successfully",
+            status: 200
+        };
+
+    } catch (error) {
+        console.error(error);
+        return {
+            message: 'Failed to fetch folder',
+            status: 500,
+        }
+    }
+}
+
+function writeFile(filePath: string, fileData: Buffer): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+        await createFolder(path.dirname(filePath));
+
+        fs.writeFile(filePath, fileData, (err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve()
+            }
+        })
+    });
+}
+
+function createFolder(dirName: string) {
+    return new Promise<void>((resolve, reject) => {
+        fs.mkdir(dirName, { recursive: true }, (err) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        });
+    })
 }
