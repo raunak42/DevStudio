a585a5722fb9adbd245d73eaac28d0d54999092a
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-24 22:52:11 +0530

    Custom height value.

--- apps/cloud-ide/tailwind.config.ts
+++ apps/cloud-ide/tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      height: {
+        'screen-minus-nav': 'calc(100vh - 65px)',
+      },
+
     },
   },
   plugins: [],
