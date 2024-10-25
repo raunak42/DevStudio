201e667cd10e3946f3ddc28c162c5dba9ffb6cf0
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-26 00:36:09 +0530

    Style changes.

--- apps/cloud-ide/src/components/SessionNavbar.tsx
+++ apps/cloud-ide/src/components/SessionNavbar.tsx
@@ -1,15 +1,17 @@
 "use client";
 import React from "react";
-import { BsWindowSidebar } from "react-icons/bs";
+import { PanelsTopLeft } from 'lucide-react';
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 
 export const SessionNavbar: React.FC = () => {
   return (
-    <div className="h-16 w-full py-8 px-16 gap-16 flex items-center justify-between z-20 border-b bg-white/80 backdrop-blur-sm">
+    <div className="h-16 w-full py-8 px-4 gap-16 flex items-center justify-between z-20 border-b bg-white/80 backdrop-blur-sm">
       <Link href={"/"} className="flex items-center gap-2">
-        <BsWindowSidebar
-          className="text-[#000000] stroke-[0.1px]"
+        <PanelsTopLeft
+        // fill="#000000"
+        color="#000000"
+          className="text-[#000000] stroke-[1.4px]"
           size={28}
         />
         <h1 className="text-2xl font-normal text-[#000000]">DevStudio</h1>
