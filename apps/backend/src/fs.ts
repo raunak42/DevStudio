9e0fcdff76d09c6eefbc8a74fd3174485f9435f1
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-26 00:37:38 +0530

    Some fs functions.

---
+++ apps/backend/src/fs.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+
+interface Entry { //File system entry is the common name for files and directories.
+    type: string;
+    name: string;
+    path: string
+}
+
+export const fetchDir = async (dir: string, baseDir: string): Promise<Entry[]> => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dir, { withFileTypes: true }, (err, entries) => {
+            if (err) {
+                reject(err)
+            }
+            else {
+                resolve(entries.map((entry) => {
+                    const entryType = entry.isDirectory() ? "dir" : "file"
+                    return {
+                        type: entryType,
+                        name: entry.name,
+                        path: `${baseDir}/${entry.name}`
+                    }
+                }))
+            }
+        })
+    })
+}
+
+export const fetchFileContent = async (file: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, "utf8", (err, data) => {
+            if (err) {
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
+}
