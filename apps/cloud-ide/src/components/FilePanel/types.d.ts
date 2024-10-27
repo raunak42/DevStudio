a40c2597893a86dc211adf4bf5e694b8bed5f7a0
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-27 23:32:29 +0530

    Created a file to house all the types related with FilePanel.

---
+++ apps/cloud-ide/src/components/FilePanel/types.d.ts
@@ -0,0 +1,50 @@
+import { Dispatch, SetStateAction } from "react";
+import { Socket } from "socket.io-client";
+
+export interface EntityConponentProps {
+    entity: entity;
+    depth: number;
+    socket: Socket | null;
+    allEntities: entity[];
+    setAllEntities: Dispatch<SetStateAction<entity[]>>;
+}
+
+export type entity = {
+    type: "dir" | "file";
+    name: string;
+    path: string;
+    children: entity[];
+};
+
+export interface getRootContentsProps {
+    socket: Socket | null;
+    setAllEntities: Dispatch<SetStateAction<entity[]>>
+}
+
+export interface clickDirProps {
+    socket: Socket | null;
+    entity: entity;
+    showOpen: boolean;
+    setShowOpen: Dispatch<SetStateAction<boolean>>;
+    setAllEntities: Dispatch<SetStateAction<entity[]>>;
+}
+
+export interface watcherAddDirProps {
+    entity: entity;
+    setAllEntities: Dispatch<SetStateAction<entity[]>>;
+    socket: Socket | null;
+}
+
+export interface getFreshDataProps {
+    socket: Socket | null;
+    entity: entity;
+    setAllEntities: Dispatch<SetStateAction<entity[]>>;
+
+}
+
+export interface useWatcherProps {
+    socket: Socket | null;
+    entity: entity;
+    setAllEntities: Dispatch<SetStateAction<entity[]>>;
+
+}
