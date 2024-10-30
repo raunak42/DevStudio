7a48397fb8742bdf8f76a3b6f47c3ef19c827652
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-10-30 13:48:45 +0530

    Loading component created.

---
+++ apps/cloud-ide/src/app/(pages)/project/loading.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
+import { PiSpinnerLight } from "react-icons/pi";
+
+export default function Loading() {
+  const [messageIndex, setMessageIndex] = useState<number>(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMessageIndex((prev) => {
+        if (prev < 3) {
+          return prev + 1;
+        }
+        return prev;
+      });
+    }, 4000);
+  }, []);
+
+  return (
+    <div className="h-screen-minus-nav w-screen flex flex-col items-center justify-start mt-64 gap-2">
+      <PiSpinnerLight
+        size={56}
+        strokeWidth={0.5}
+        className="animate-spin text-gray-600"
+      />
+      {loadingMessages[messageIndex]}
+    </div>
+  );
+}
+
+const loadingMessages = [
+  <h1 key={0} className="text-sm">
+    Preparing workspace.
+  </h1>,
+  <h1 key={1} className="text-sm">
+    Fetching your files.
+  </h1>,
+  <h1 key={2} className="text-sm">
+    Setting up the terminal.
+  </h1>,
+  <h1 key={2} className="text-sm">
+    Just hang in there.
+  </h1>,
+];
