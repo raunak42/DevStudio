92b79bc12f3f5bbb59a5dd75c17f38c75c863cac
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-11-02 14:50:13 +0530

    Minor changes.

--- apps/cloud-ide/src/components/CodePanel.tsx
+++ apps/cloud-ide/src/components/CodePanel.tsx
@@ -1,5 +1,8 @@
 import { Panel } from "react-resizable-panels";
 
 export const CodePanel: React.FC = () => {
-  return <Panel className="rounded-3xl" defaultSize={44}></Panel>;
+  return (
+    <Panel className="rounded-3xl p-8" defaultSize={46}>
+    </Panel>
+  );
 };
