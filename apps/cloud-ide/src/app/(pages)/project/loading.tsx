c12ab68826c711a056c5328df9b20447321fe393
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-11-02 14:46:38 +0530

    Minor changes.

--- apps/cloud-ide/src/app/(pages)/project/loading.tsx
+++ apps/cloud-ide/src/app/(pages)/project/loading.tsx
@@ -6,15 +6,17 @@ export default function Loading() {
   const [messageIndex, setMessageIndex] = useState<number>(0);
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setMessageIndex((prev) => {
-        if (prev < 3) {
+        if (prev < loadingMessages.length - 1) {
           return prev + 1;
         }
         return prev;
       });
     }, 4000);
-  }, []);
+
+    return () => clearTimeout(timer);
+  }, [messageIndex]); 
 
   return (
     <div className="h-screen-minus-nav w-screen flex flex-col items-center justify-start mt-64 gap-2">
@@ -30,15 +32,15 @@ export default function Loading() {
 
 const loadingMessages = [
   <h1 key={0} className="text-sm">
-    Preparing workspace.
+    Preparing workspace...
   </h1>,
   <h1 key={1} className="text-sm">
-    Fetching your files.
+    Fetching your files...
   </h1>,
   <h1 key={2} className="text-sm">
-    Setting up the terminal.
+    Setting up the terminal...
   </h1>,
-  <h1 key={2} className="text-sm">
-    Just hang in there.
+  <h1 key={3} className="text-sm">
+    Just hang in there...
   </h1>,
 ];
