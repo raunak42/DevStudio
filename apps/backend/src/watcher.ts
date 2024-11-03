029ed1c865d9624accfe248efda3b5368f631e16
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-11-03 15:49:39 +0530

    New fucntions for adding and deleting files to and from respectively from s3 when watcher observer add and unlink.

--- apps/backend/src/watcher.ts
+++ apps/backend/src/watcher.ts
@@ -1,14 +1,25 @@
 import chokidar from 'chokidar';
 import { Socket } from 'socket.io';
+import { addNewToS3, deleteFromS3 } from './aws';
 
-const modifyPath = (path: string) => {
+const modifyPathForClient = (path: string) => {
     const pathArray = path.split("workspace")
     const modifiedPath = pathArray[1]
     return modifiedPath
 }
 
+const modifyPathForS3 = (path: string, projectId: string, userId: string) => {
+    const workspaceName = projectId.split("-")[1]
+    const x = path.split(workspaceName)[1]
+    const pathForS3 = `user/${userId}/${projectId}${x}`
+    return pathForS3
+}
+
 export function watchDirectory(directoryPath: string, socket: Socket | null) {
     if (!socket) return
+    const userId = socket.handshake.query.userId as string
+    const projectId = socket.handshake.query.projectId as string
+
     const watcher = chokidar.watch(directoryPath, {
         persistent: true,
         ignoreInitial: true
@@ -17,10 +28,19 @@ export function watchDirectory(directoryPath: string, socket: Socket | null) {
     const events = ['add', 'addDir', 'unlink', 'unlinkDir']
 
     events.map((event) => {
-        watcher.on(event, (path) => {
-            const modifiedPath = modifyPath(path)
+        watcher.on(event, async (path) => {
+            const modifiedPath = modifyPathForClient(path)
             socket.emit("watcherEvent", { path: modifiedPath, event })
+
+            const pathForS3 = modifyPathForS3(path, projectId, userId)
+            if (event === "add") {
+                await addNewToS3(pathForS3)
+            }
+            else if (event === "unlink") {
+                await deleteFromS3(pathForS3)
+            }
         })
+
     })
 
     watcher.on('error', error => {
