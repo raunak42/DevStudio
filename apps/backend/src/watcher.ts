54069fff1f5618a22a2d7d80ea3fb14b4b9896de
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-11-07 14:23:57 +0530

    Modified to add/delete folders from s3 too.

--- apps/backend/src/watcher.ts
+++ apps/backend/src/watcher.ts
@@ -8,7 +8,7 @@ const modifyPathForClient = (path: string) => {
     return modifiedPath
 }
 
-const modifyPathForS3 = (path: string, projectId: string, userId: string) => {
+export const modifyPathForS3 = (path: string, projectId: string, userId: string) => {
     const workspaceName = projectId.split("-")[1]
     const x = path.split(workspaceName)[1]
     const pathForS3 = `user/${userId}/${projectId}${x}`
@@ -34,20 +34,29 @@ export function watchDirectory(directoryPath: string, socket: Socket | null) {
 
             const pathForS3 = modifyPathForS3(path, projectId, userId)
             if (event === "add") {
-                await addNewToS3(pathForS3)
+                await addNewToS3({ path: pathForS3, type: "file" })
+            }
+            else if (event === "addDir") {
+                await addNewToS3({ path: pathForS3, type: "dir" })
             }
             else if (event === "unlink") {
-                await deleteFromS3(pathForS3)
+                await deleteFromS3({ path: pathForS3, type: "file" })
+            }
+            else if (event === "unlinkDir") {
+                await deleteFromS3({ path: pathForS3, type: "dir" })
             }
         })
 
+    })
+
+    watcher.on('all', (event, path) => {
+        console.log(event)
     })
 
     watcher.on('error', error => {
         console.error('Error occurred', error);
     });
 
-    return watcher; // Return watcher instance so it can be closed if needed
+    return watcher;
 }
 
-// watcher.close();
